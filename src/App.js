605878ba1bc73c63ac9e43a3a6ed66d8080d5004
Joshua Stupplebeen <jpstup@gmail.com>
2018-07-10 22:31:30 -0600

    board setup working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import circle from './circle.svg';
-import board from './board.svg';
+import Board from './Board';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      // <div className="App">
+      //   <img src={board} className="App-board" />
+      // </div>
       <div className="App">
-        <img src={board} className="App-board" />
+        <Board />
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,83 @@
 import React, { Component } from 'react';
 
-export default Board extends Component {
+export default class Board extends Component {
   render() {
     return (
-      <div />
+      <svg className="App-board">
+        <rect width="100%" height="100%" x="20" y="0" fill="yellow" />
+
+        <text x="150" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("1")}
+          className="App-column">1</text>
+        <text x="300" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("2")}
+          className="App-column">2</text>
+        <text x="450" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("3")}
+          className="App-column">3</text>
+        <text x="600" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("4")}
+          className="App-column">4</text>
+        <text x="750" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("5")}
+          className="App-column">5</text>
+        <text x="900" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("6")}
+          className="App-column">6</text>
+        <text x="1050" y="40" font-size="30" text-anchor="middle" fill="black"
+          onClick={() => console.log("7")}
+          className="App-column">7</text>
+
+        <circle cx="150" cy="100" r="35" fill="grey" />
+        <circle cx="300" cy="100" r="35" fill="grey" />
+        <circle cx="450" cy="100" r="35" fill="grey" />
+        <circle cx="600" cy="100" r="35" fill="grey" />
+        <circle cx="750" cy="100" r="35" fill="grey" />
+        <circle cx="900" cy="100" r="35" fill="grey" />
+        <circle cx="1050" cy="100" r="35" fill="grey" />
+
+        <circle cx="150" cy="220" r="35" fill="grey" />
+        <circle cx="300" cy="220" r="35" fill="grey" />
+        <circle cx="450" cy="220" r="35" fill="grey" />
+        <circle cx="600" cy="220" r="35" fill="grey" />
+        <circle cx="750" cy="220" r="35" fill="grey" />
+        <circle cx="900" cy="220" r="35" fill="grey" />
+        <circle cx="1050" cy="220" r="35" fill="grey" />
+
+        <circle cx="150" cy="340" r="35" fill="grey" />
+        <circle cx="300" cy="340" r="35" fill="grey" />
+        <circle cx="450" cy="340" r="35" fill="grey" />
+        <circle cx="600" cy="340" r="35" fill="grey" />
+        <circle cx="750" cy="340" r="35" fill="grey" />
+        <circle cx="900" cy="340" r="35" fill="grey" />
+        <circle cx="1050" cy="340" r="35" fill="grey" />
+
+        <circle cx="150" cy="460" r="35" fill="grey" />
+        <circle cx="300" cy="460" r="35" fill="grey" />
+        <circle cx="450" cy="460" r="35" fill="grey" />
+        <circle cx="600" cy="460" r="35" fill="grey" />
+        <circle cx="750" cy="460" r="35" fill="grey" />
+        <circle cx="900" cy="460" r="35" fill="grey" />
+        <circle cx="1050" cy="460" r="35" fill="grey" />
+
+        <circle cx="150" cy="580" r="35" fill="grey" />
+        <circle cx="300" cy="580" r="35" fill="grey" />
+        <circle cx="450" cy="580" r="35" fill="grey" />
+        <circle cx="600" cy="580" r="35" fill="grey" />
+        <circle cx="750" cy="580" r="35" fill="grey" />
+        <circle cx="900" cy="580" r="35" fill="grey" />
+        <circle cx="1050" cy="580" r="35" fill="grey" />
+
+        <circle cx="150" cy="700" r="35" fill="grey" />
+        <circle cx="300" cy="700" r="35" fill="grey" />
+        <circle cx="450" cy="700" r="35" fill="grey" />
+        <circle cx="600" cy="700" r="35" fill="grey" />
+        <circle cx="750" cy="700" r="35" fill="grey" />
+        <circle cx="900" cy="700" r="35" fill="grey" />
+        <circle cx="1050" cy="700" r="35" fill="grey" />
+
+      </svg>
+
     );
   }
 }
