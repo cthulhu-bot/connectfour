7ce256db0bd424c25fb6ad786530e595feb5a9d3
Joshua Stupplebeen <jpstup@gmail.com>
2018-07-12 00:51:30 -0600

    working on playerTurn func

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import Board from './Board';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.gameModel = {
+      winner: 'none',
+      currentPlayer: 'Player 1',
+      boardModel: [0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0]
+    };
+    this.state = {
+      gameModel: this.gameModel,
+    }
+  }
+
+  playerTurn = (columnNum) => () => {
+    const currentPlayer = this.gameModel.currentPlayer;
+    this.gameModel.currentPlayer = currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1';
+    console.log(columnNum)
+    this.setState({
+      gameModel: this.gameModel
+    })
+  }
+
   render() {
     return (
-      // <div className="App">
-      //   <img src={board} className="App-board" />
-      // </div>
       <div className="App">
-        <Board />
+        <Board playerTurn={this.playerTurn} />
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -6,26 +6,26 @@ export default class Board extends Component {
       <svg className="App-board">
         <rect width="100%" height="100%" x="20" y="0" fill="yellow" />
 
-        <text x="150" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("1")}
+        <text x="150" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(1)}
           className="App-column">1</text>
-        <text x="300" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("2")}
+        <text x="300" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(2)}
           className="App-column">2</text>
-        <text x="450" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("3")}
+        <text x="450" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(3)}
           className="App-column">3</text>
-        <text x="600" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("4")}
+        <text x="600" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(4)}
           className="App-column">4</text>
-        <text x="750" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("5")}
+        <text x="750" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(5)}
           className="App-column">5</text>
-        <text x="900" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("6")}
+        <text x="900" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(6)}
           className="App-column">6</text>
-        <text x="1050" y="40" font-size="30" text-anchor="middle" fill="black"
-          onClick={() => console.log("7")}
+        <text x="1050" y="40" fontSize="30" textAnchor="middle" fill="black"
+          onClick={this.props.playerTurn(7)}
           className="App-column">7</text>
 
         <circle cx="150" cy="100" r="35" fill="grey" />
