de3f1aa0c4284f58bc2696a4abec3b73f13a5baf
Joshua Stupplebeen <jpstup@gmail.com>
2018-07-12 19:55:19 -0600

    working on board logic

--- src/App.js
+++ src/App.js
@@ -22,10 +22,31 @@ class App extends Component {
     }
   }
 
+  placePiece = (column, boardModel, player) => {
+    let newBoardModel = boardModel;
+    const columnSlots = boardModel.filter((slot, idx) => idx % (column - 1) === 0);
+    const columnFilled = columnSlots.every((slot, idx) => slot !== 0);
+    if (!columnFilled) {
+      const firstEmptySlot = columnSlots.reduce((acc, slot, idx) => {
+        return slot === 0 ? idx : acc;
+      }, 0);
+      newBoardModel[(column - 1) + (7 * firstEmptySlot)] = player === 'Player 1' ? 1 : 2;
+    } else {
+      throw 'that column is already filled';
+    }
+    return newBoardModel;
+  }
+
   playerTurn = (columnNum) => () => {
     const currentPlayer = this.gameModel.currentPlayer;
     this.gameModel.currentPlayer = currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1';
-    console.log(columnNum)
+
+    try {
+      this.gameModel.boardModel = this.placePiece(columnNum, this.gameModel.boardModel, this.gameModel.currentPlayer);
+    } catch (e) {
+      
+    }
+
     this.setState({
       gameModel: this.gameModel
     })
