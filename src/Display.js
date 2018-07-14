e0660a931257028e0be2090fc735f45e3fb2f578
Joshua Stupplebeen <jpstup@gmail.com>
2018-07-14 13:41:05 -0600

    working on display

--- src/Display.js
+++ src/Display.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
 
 export default class Display extends Component {
     render() {
         return (
-            <div>{`Winner: ${this.props.gameModel.winner || 'none'}`}</div>
+            <div>
+                <div>Please click on the column</div>
+                <div>headers to play</div>
+                <div className='App-display-winner'>{`Winner: ${this.props.gameModel.winner || 'none'}`}</div>
+            </div>
         )
     }
 }
